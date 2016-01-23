14ec23fdd93ba2d795b1cc3c30aefa739f610c9a
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-01-23 09:34:46 -0500

    git commit changed send alert to include long and lat

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,12 +1,3 @@
 class Member < ActiveRecord::Base
     belongs_to :user
-    
-    def send_message(message) 
-        msg = $twilio.account.messages.create(
-            :from => $twilio_number,
-            :to => number,
-            :body => message
-        )
-        puts msg.to
-    end
 end
